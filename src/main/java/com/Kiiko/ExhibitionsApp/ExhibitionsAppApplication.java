b692a1c0375529d1dbdf4e632f045d687b543aa3
Vasyl Kiiko <vasylkiikofw@gmail.com>
2021-04-12 11:25:08 +0300

    Initial commit

---
+++ src/main/java/com/Kiiko/ExhibitionsApp/ExhibitionsAppApplication.java
@@ -0,0 +1,13 @@
+package com.Kiiko.ExhibitionsApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExhibitionsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExhibitionsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Kiiko/ExhibitionsApp/ExhibitionsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kiiko.ExhibitionsApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExhibitionsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
